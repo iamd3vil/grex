95203140c42bf76e1107e98b212769027dcaebfd
Sarat Chandra <me@saratchandra.in>
2019-09-03 17:11:30 +0530

    Add delegates

--- lib/grex.ex
+++ lib/grex.ex
@@ -2,4 +2,9 @@ defmodule Grex do
   @moduledoc """
   Goodreads API Client. Currently only a subset of APIs are implemented.
   """
+
+  defdelegate new(key), to: Grex.Client
+
+  defdelegate get_reviews(client, gr_id, shelf, sort, search, order, page, per_page),
+    to: Grex.Reviews
 end
